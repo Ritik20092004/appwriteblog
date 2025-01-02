778d6d3ca07e8fddcd43b44301211159b423ee4d
Ritik20092004 <ritiktiwari4703@gmail.com>
2025-01-02 20:06:58 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer/Footer"
+import Header from "./Header/Header"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from './AuthLayout'
+import Select from "./Select"
+import Input from "./input"
+import Button from "./Button"
+
+export {Header,AuthLayout ,Footer,Container,Logo, PostCard ,PostForm , Login,Signup,LogoutBtn , Input, Button ,RTE,Select} 
